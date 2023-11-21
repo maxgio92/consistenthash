961a37db31da6c673b250f568643c9a32de11bbe
Massimiliano Giovagnoli <me@maxgio.it>
2023-11-21 13:56:46 +0100

    test: rewrite tests with bdd via ginkgo
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- consistent_test.go
+++ consistent_test.go
@@ -1,7 +1,10 @@
-package consistenthash
+package consistenthash_test
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"consistenthash"
 )
 
 var (
@@ -10,97 +13,154 @@ var (
 	node3id = "node3"
 )
 
-func TestAddNode(t *testing.T) {
-	Convey("Given empty ring", t, func() {
-		Convey("Then it should add node", func() {
-			r := NewRing()
-			r.AddNode(node1id)
+var _ = Describe("Adding nodes", func() {
+	Context("With one node", Ordered, func() {
+		var r *consistenthash.Ring
 
-			So(r.Nodes.Len(), ShouldEqual, 1)
+		r = consistenthash.NewRing()
 
-			Convey("Then node should've hashed id", func() {
-				So(r.Nodes[0].HashId, ShouldHaveSameTypeAs, uint32(0))
-			})
+		BeforeAll(func() {
+			r.AddNode("")
 		})
+		It("Should add one node", func() {
+			Expect(r.Nodes.Len()).To(Equal(1))
+		})
+		It("Should generate a valid node hash", func() {
+			Expect(r.Nodes[0].HashId).To(Equal(uint32(0)))
+		})
+	})
+	Context("With multiple nodes", Ordered, func() {
+		var r *consistenthash.Ring
 
-		Convey("Then it should add node & sort by node id", func() {
-			r := NewRing()
+		r = consistenthash.NewRing()
+
+		BeforeAll(func() {
 			r.AddNode(node1id)
 			r.AddNode(node2id)
-
-			So(r.Nodes.Len(), ShouldEqual, 2)
-
-			node1hash := hashId(node1id)
-			node2hash := hashId(node2id)
-
-			So(node1hash, ShouldBeGreaterThan, node2hash)
-
-			So(r.Nodes[0].Id, ShouldEqual, node2id)
-			So(r.Nodes[1].Id, ShouldEqual, node1id)
+			r.AddNode(node3id)
 		})
-	})
-}
+		It("Should add nodes", func() {
+			Expect(r.Nodes.Len()).To(Equal(3))
+		})
+		It("Should sort nodes by node hash", func() {
+			Expect(r.Nodes[0].HashId).To(BeNumerically("<", r.Nodes[1].HashId))
+			Expect(r.Nodes[1].HashId).To(BeNumerically("<", r.Nodes[2].HashId))
 
-func TestRemoveNode(t *testing.T) {
-	Convey("Given ring with nodes", t, func() {
-		r := NewRing()
+			Expect(r.Nodes[0].Id).To(Equal(node2id))
+			Expect(r.Nodes[1].Id).To(Equal(node3id))
+			Expect(r.Nodes[2].Id).To(Equal(node1id))
+		})
+
+	})
+})
+
+var _ = Describe("Removing nodes", func() {
+	Context("Given ring with nodes", func() {
+		var r *consistenthash.Ring
+
+		r = consistenthash.NewRing()
 		r.AddNode(node1id)
 		r.AddNode(node2id)
 		r.AddNode(node3id)
 
-		Convey("When node doesn't exist", func() {
-			Convey("Then it should return error", func() {
-				err := r.RemoveNode("nonexistent")
-				So(err, ShouldEqual, ErrNodeNotFound)
+		When("The node exists", Ordered, func() {
+			var err error
+			BeforeAll(func() {
+				err = r.RemoveNode(node2id)
+			})
+			It("Should not return error", func() {
+				Expect(err).To(BeNil())
+			})
+			It("Should remove the node", func() {
+				Expect(r.Nodes.Len()).To(Equal(2))
+				Expect(r.Nodes[0].Id).To(Equal(node3id))
+				Expect(r.Nodes[1].Id).To(Equal(node1id))
 			})
 		})
-
-		Convey("When node exists", func() {
-			Convey("Then it should remove node", func() {
-				err := r.RemoveNode(node2id)
-				So(err, ShouldBeNil)
-
-				So(r.Nodes.Len(), ShouldEqual, 2)
-
-				So(r.Nodes[0].Id, ShouldEqual, node3id)
-				So(r.Nodes[1].Id, ShouldEqual, node1id)
+		When("The node does not exist", Ordered, func() {
+			var err error
+			BeforeAll(func() {
+				err = r.RemoveNode("nonexistent")
+			})
+			It("Should return error", func() {
+				Expect(err).ToNot(BeNil())
 			})
 		})
 	})
-}
+})
 
-func TestGet(t *testing.T) {
-	Convey("Given ring with 1 node", t, func() {
-		r := NewRing()
+var _ = Describe("Getting node", func() {
+	Context("Given ring with 1 node", Ordered, func() {
+		var (
+			r                              *consistenthash.Ring
+			key1, key2, key1Node, key2Node string
+		)
+
+		r = consistenthash.NewRing()
 		r.AddNode(node1id)
 
-		Convey("Then it should return that node regardless of input", func() {
-			insertnode := r.Get("id")
-			So(insertnode, ShouldEqual, node1id)
+		key1 = "key1"
+		key2 = "key2"
 
-			insertnode = r.Get("anykey")
-			So(insertnode, ShouldEqual, node1id)
+		BeforeAll(func() {
+			key1Node = r.Get(key1)
+			key2Node = r.Get(key2)
+		})
+		It("Should always return that node", func() {
+			Expect(key1Node).To(Equal(node1id))
+			Expect(key2Node).To(Equal(node1id))
 		})
 	})
+	Context("Given ring with multiple nodes", Ordered, func() {
+		var (
+			r              *consistenthash.Ring
+			key1, key1Node string
+			key2, key2Node string
+			key3, key3Node string
+			key4, key4Node string
+		)
 
-	Convey("Given ring with multiple nodes", t, func() {
-		insertid := "justa"
-
-		r := NewRing()
+		r = consistenthash.NewRing()
 		r.AddNode(node1id)
 		r.AddNode(node2id)
 		r.AddNode(node3id)
 
-		Convey("Then it should return node closest", func() {
-			node1hash := hashId(node1id)
-			node3hash := hashId(node2id)
-			inserthash := hashId(insertid)
+		key1 = "justa"
+		key2 = "justb"
+		key3 = "justc"
+		key4 = "justd"
 
-			So(inserthash, ShouldBeLessThan, node1hash)
-			So(inserthash, ShouldBeGreaterThan, node3hash)
+		BeforeAll(func() {
+			key1Node = r.Get(key1)
+			key2Node = r.Get(key2)
+			key3Node = r.Get(key3)
+			key4Node = r.Get(key4)
+		})
+		It("Should return the closest node", func() {
+			Expect(key1Node).To(Equal(node1id))
+			Expect(key2Node).To(Equal(node2id))
+			Expect(key3Node).To(Equal(node1id))
+			Expect(key4Node).To(Equal(node3id))
+		})
 
-			insertnode := r.Get(insertid)
-			So(insertnode, ShouldEqual, node1id)
+		Context("Removing a node", Ordered, func() {
+			BeforeAll(func() {
+				r.RemoveNode(node3id)
+
+				key1Node = r.Get(key1)
+				key2Node = r.Get(key2)
+				key3Node = r.Get(key3)
+				key4Node = r.Get(key4)
+			})
+
+			It("The keys of the remaining nodes should not be re-assigned", func() {
+				Expect(key1Node).To(Equal(node1id))
+				Expect(key2Node).To(Equal(node2id))
+				Expect(key3Node).To(Equal(node1id))
+			})
+			It("The keys of the removed node should be reassigned to the next closest node", func() {
+				Expect(key4Node).To(Equal(node1id))
+			})
 		})
 	})
-}
+})
---
+++ consistenthash_suite_test.go
@@ -0,0 +1,13 @@
+package consistenthash_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestGoConsistenthash(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "GoConsistenthash Suite")
+}

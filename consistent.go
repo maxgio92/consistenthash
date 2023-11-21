a5fc2be1f8403c0b74171f57ad276bee5e381699
Massimiliano Giovagnoli <me@maxgio.it>
2023-11-21 13:55:37 +0100

    refactor: add multiple reader single writer lock
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- consistent.go
+++ consistent.go
@@ -9,9 +9,11 @@ import (
 
 var ErrNodeNotFound = errors.New("node not found")
 
+// Ring is a network of distributed nodes.
 type Ring struct {
 	Nodes Nodes
-	sync.Mutex
+
+	sync.RWMutex
 }
 
 func NewRing() *Ring {
@@ -19,9 +21,6 @@ func NewRing() *Ring {
 }
 
 func (r *Ring) AddNode(id string) {
-	r.Lock()
-	defer r.Unlock()
-
 	node := NewNode(id)
 	r.Nodes = append(r.Nodes, node)
 
@@ -42,8 +41,13 @@ func (r *Ring) RemoveNode(id string) error {
 	return nil
 }
 
-func (r *Ring) Get(id string) string {
-	i := r.search(id)
+// Get returns the node closest to the key specified as argument,
+// from the nodes ring.
+func (r *Ring) Get(key string) string {
+	r.RLock()
+	defer r.RUnlock()
+
+	i := r.search(key)
 	if i >= r.Nodes.Len() {
 		i = 0
 	}
@@ -51,18 +55,21 @@ func (r *Ring) Get(id string) string {
 	return r.Nodes[i].Id
 }
 
+// search is a binary search in the ring.
+// It's implemented with sort.Search binary search.
 func (r *Ring) search(id string) int {
-	searchfn := func(i int) bool {
+	searchFn := func(i int) bool {
 		return r.Nodes[i].HashId >= hashId(id)
 	}
 
-	return sort.Search(r.Nodes.Len(), searchfn)
+	return sort.Search(r.Nodes.Len(), searchFn)
 }
 
 //----------------------------------------------------------
 // Node
 //----------------------------------------------------------
 
+// Node is a single entity in a ring.
 type Node struct {
 	Id     string
 	HashId uint32
@@ -77,9 +84,10 @@ func NewNode(id string) *Node {
 
 type Nodes []*Node
 
+// Nodes implement sort.Interface.
 func (n Nodes) Len() int           { return len(n) }
-func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
 func (n Nodes) Less(i, j int) bool { return n[i].HashId < n[j].HashId }
+func (n Nodes) Swap(i, j int)      { n[i], n[j] = n[j], n[i] }
 
 //----------------------------------------------------------
 // Helpers
